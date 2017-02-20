5c7d834f0d2b28521438b92648f5d2add38d4a51
rgarro <rolando@emptyart.xyz>
2017-02-20 12:16:36 -0600

    update ..

---
+++ 3D/Essential/_02.js
@@ -0,0 +1,11 @@
+function _02(p){
+  Basic_3D_Template.call(this.p);
+}
+
+_02.prototype = Object.create(Basic_3D_Template.prototype);
+
+_02.prototype.constructor = _02;
+
+_01.prototype.postInit = function(){
+  
+}
