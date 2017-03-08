a2196aef1bfb4df881776f81d7b0ad6fa19277cc
rgarro <rolando@emptyart.xyz>
2017-03-07 19:10:21 -0600

    casting a tank ...

--- 3D/Games/Kalero/Kalero.js
+++ 3D/Games/Kalero/Kalero.js
@@ -23,18 +23,18 @@ Kalero.prototype.postInit = function(){
   var p = this;
   //var loader = new THREE.ObjectLoader();
   var loader = new THREE.JSONLoader();
-loader.load("/emptyLibJS/3D/Games/Kalero/assets/a3.json",function(model,materials){
+loader.load("/emptyLibJS/3D/Games/Kalero/assets/ultimate-tank.json",function(model,materials){
 //console.log(model);
     var material = new THREE.MeshPhongMaterial();
     //var material = new THREE.MultiMaterial(materials);
-    material.color = 0xF47a42;
+    //material.color = 0xF47a42;
     var mesh = new THREE.Mesh(model,material);
     //mesh.translateY(-0.5);
     //mesh.scale = new THREE.Vector3(3,3,3);
     p.scene.add(mesh);
 
     // now add some better lighting
-    var ambientLight = new THREE.AmbientLight(0x111111);
+    var ambientLight = new THREE.AmbientLight(0xF47a42);
     ambientLight.name='ambient';
     p.scene.add(ambientLight);
 
