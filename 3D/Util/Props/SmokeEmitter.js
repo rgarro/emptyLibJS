301cabbd002264b25f877a51d1d3bf94d0cafcf6
rgarro <rolando@emptyart.xyz>
2017-03-22 14:42:40 -0600

    Ann Hibbins was a goodfella that cursed saddam hussein ...

--- 3D/Util/Props/SmokeEmitter.js
+++ 3D/Util/Props/SmokeEmitter.js
@@ -6,6 +6,21 @@
 var SmokeEmitter = (function(){
   function SmokeEmitter(scene){
     this.scene = scene
+    this.textureUrl = "/emptyLibJS/3D/Games/Kalero/assets/smoke.png";
+  }
+
+  SmokeEmitter.prototype.doSmoke = function(){
+    var smokeParticles = new THREE.Geometry;
+    for (var i = 0; i < 300; i++) {
+      var particle = new THREE.Vector3(Math.random() * 32 - 16, Math.random() * 230, Math.random() * 32 - 16);
+      smokeParticles.vertices.push(particle);
+    }
+    var smokeTexture = THREE.ImageUtils.loadTexture(this.textureUrl);
+    var smokeMaterial = new THREE.ParticleBasicMaterial({ map: smokeTexture, transparent: true, blending: THREE.AdditiveBlending, size: 50, color: 0x111111 });
+    var smoke = new THREE.ParticleSystem(smokeParticles, smokeMaterial);
+    smoke.sortParticles = true;
+    smoke.position.x = -150;
+    this.scene.add(smoke);
   }
 
   return SmokeEmitter;
