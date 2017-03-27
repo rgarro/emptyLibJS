a617e91bd46fbb4a0269e8f19a2ab07ada9f5b60
rgarro <rolando@emptyart.xyz>
2017-03-27 13:05:16 -0600

    gravity template update

--- 3D/Templates/Basic_3D_Gravity.js
+++ 3D/Templates/Basic_3D_Gravity.js
@@ -16,15 +16,44 @@
  *        "-.._____.,-" .                    .
  *+           .                .   +                       .
  *+          .             +                                  .
- *.             .      .      
+ *.             .      .
  *
  *
  * @author Rolando <rolando@emptyart.xyz>
  */
 var Basic_3D_Gravity = (function(){
   function Basic_3D_Gravity(){
-
+    Physijs.scripts.worker = '/libs/physijs_worker.js';
+  	Physijs.scripts.ammo = '/libs/ammo.js';
+    this.container = null;
+    this.renderer = null;
+    this.scene = null;
+    this.camera = null;
+    this.ini_camera_x = 15;
+    this.ini_camera_y = 16;
+    this.ini_camera_z =13;
+    this.is_camera_set = false;
+    this.cameraControl = null;
+    this.show_control_gui = false;
+    this.show_stats = false;
+    this.ds = null;//datastats object
+    this.clearColor = 0x000000;
+    this.l = new Light();
+    if(typeof arguments[0] != 'undefined'){
+      this.setContainer(arguments[0]);
+    }
   }
+
+  Basic_3D_Gravity.prototype.setContainer = function(containerID){
+    if(typeof containerID == 'string'){
+      this.container = document.getElementById(containerID);
+      if(this.container === null){
+        throw new Error("Container doesn exist.");
+      }
+    }else{
+      throw new Error("ContainerID must be a valid String");
+    }
+  };
 
   return Basic_3D_Gravity;
 })();
