41e158947d27c8c0ee3af928fd522ec3852ada80
rgarro <rolando@emptyart.xyz>
2017-04-07 17:47:15 -0600

    bubbles wants a new gun

--- 3D/Util/Props/Throwable.js
+++ 3D/Util/Props/Throwable.js
@@ -9,6 +9,24 @@ var Throwable = (function(){
     this.modelUrl = null;
   }
 
+  Throwable.prototype.doThrow = function(){
+    // Creates a ball and throws it
+				var ballMass = 35;
+				var ballRadius = 0.4;
+				var ball = new THREE.Mesh( new THREE.SphereGeometry( ballRadius, 14, 10 ), ballMaterial );
+				ball.castShadow = true;
+				ball.receiveShadow = true;
+				var ballShape = new Ammo.btSphereShape( ballRadius );
+				ballShape.setMargin( margin );
+				pos.copy( raycaster.ray.direction );
+				pos.add( raycaster.ray.origin );
+				quat.set( 0, 0, 0, 1 );
+				var ballBody = createRigidBody( ball, ballShape, ballMass, pos, quat );
+				pos.copy( raycaster.ray.direction );
+				pos.multiplyScalar( 24 );
+				ballBody.setLinearVelocity( new Ammo.btVector3( pos.x, pos.y, pos.z ) );
+  }
+
   return Throwable;
 })();
 
