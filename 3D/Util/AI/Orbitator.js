9df36cec1482a6d1792b2c36f73af94d05f12445
rgarro <rolando@emptyart.xyz>
2017-04-17 10:55:09 -0600

    mexican villagers have a zeppelin factory next to this meteor crater lake ...

--- 3D/Util/AI/Orbitator.js
+++ 3D/Util/AI/Orbitator.js
@@ -13,10 +13,25 @@
 var Orbitator = (function(){
 
   function Orbitator(){
-
+    this.modelUrl = "/emptyLibJS/3D/Games/Kalero/assets/mi28/Mi28.json";
+    this.game = null;
+    this.geometry = null;
+    this.mesh = null;
+    this.gameIsSet = false;
+    this.meshName = "";
+    this.centerMeshName = "";
+    this.radiusLength = 300;
+    this.altitude = 300;
   }
 
+  Orbitator.prototype.setGame = function(game){
+    this.game = game;
+    this.gameIsSet = true;
+  }
 
+  Orbitator.prototype.loadModel = function(modelUrl){
+    
+  }
 
   return Orbitator;
 })();
