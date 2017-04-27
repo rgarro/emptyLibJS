ccde090d697f54152f0f9e5682733586085ce742
rgarro <rolando@emptyart.xyz>
2017-04-26 18:00:51 -0600

    will have elevation and turret rotation

---
+++ 3D/Util/AI/selfCenteredVerticalRotator.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Rolando <rolando@emptyart.xyz>
+ */
+var selfCenteredVerticalRotator = (function(){
+
+  function selfCenteredVerticalRotator(){
+
+  }
+
+
+  return selfCenteredVerticalRotator;
+})();
+eO._3D.Util.AI.selfCenteredVerticalRotator = selfCenteredVerticalRotator;
---
+++ specs/3D/Util/AI/selfCenteredVerticalRotatorSpec.js
@@ -0,0 +1,9 @@
+describe("selfCenteredVerticalRotator ... ",function(){
+
+
+	beforeAll(function(){
+
+	});
+
+
+});
