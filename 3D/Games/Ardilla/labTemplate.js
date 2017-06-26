bb87e92cb26512c3a7e20884ba6451d4a6bf1be8
rgarro <rolando@emptyart.xyz>
2017-06-25 19:54:25 -0600

    testing physics

--- 3D/Games/Ardilla/labTemplate.js
+++ 3D/Games/Ardilla/labTemplate.js
@@ -1,6 +1,5 @@
 /**
- * una ardilla de 0.73 kg cae de un poste de luz y llega al suelo
- * en la mitad que dura el canto de un sanate negro  (1.7s) en el mes de junio  ...
+ * replace Ardilla ...
  *
  * @author Rolando <rolando@emptyart.xyz>
  */
@@ -16,18 +15,10 @@ function Ardilla(){
   this.clearColor = 0xa5b6c5;
   this.l = new Light();
 
-  //this.planet = new Wo.Planet();
+  this.planet = new Wo.Planet();
   this.enable_shadows = true;
   this.Helicoptero = null;
-  this.ball = null;
   this.cameraControl = null;
-
-  this.g = 0.1;//acceleration due gravity
-  this.x = 50;
-  this.y = 80;
-  this.vx = 2;
-  this.vy = 0;
-  this.radius = 50;
   Game.call(this.p);
 }
 
@@ -40,27 +31,6 @@ Ardilla.prototype.postInit = function(){
   this.Helicoptero = new Helicoptero();
   this.Helicoptero.setGame(this);
   this.Helicoptero.loadModel(this.Helicoptero.modelUrl);
-  this.initBouncingBall();
-}
-
-Ardilla.prototype.initBouncingBall = function(){
-  var geometry = new THREE.SphereGeometry(5,5,5);
-  var material = new THREE.MeshBasicMaterial({color:0xefa526});
-  this.ball = new THREE.Mesh(geometry,material);
-  this.ball.position.y = this.y;
-  this.scene.add(this.ball);
-}
-
-Ardilla.prototype.renderBouncingBall = function(){
-  this.vx += this.g;
-  this.x -= this.vx;
-  this.y -= this.vy;
-  if(this.y < 0){
-    this.y = 10;
-    this.vy *= -0.8;
-  }
-  this.ball.position.y = this.y;
-  this.ball.position.x = this.x;
 }
 
 Ardilla.prototype.setControl = function(){
@@ -71,6 +41,6 @@ Ardilla.prototype.preRender = function(){
   this.cameraControl.update();
 //  this.scene.getObjectByName('ambient').color = new THREE.Color(0x111111);
   //this.scene.getObjectByName('directional').color = new THREE.Color(0xffffff);
-  //this.planet.onRender();
-  this.renderBouncingBall();
+  this.planet.onRender();
+
 }
