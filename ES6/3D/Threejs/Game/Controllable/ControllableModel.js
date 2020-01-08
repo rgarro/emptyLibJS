aee54f2fa2eba6a25520bd2415424a20af9da3c1
Rolando Garro <rolando@emptyart.xyz>
2020-01-08 16:52:18 -0600

    Dance into the fire To fatal sounds of broken dreams …
    Dance into the fire To fatal sounds of broken dreams …

--- ES6/3D/Threejs/Game/Controllable/ControllableModel.js
+++ ES6/3D/Threejs/Game/Controllable/ControllableModel.js
@@ -2,6 +2,7 @@ class ControllableModel {
   constructor() {
     this.vehicleMeshName = "meshNames";
     this.modelUrl = "/google/model.json";
+    this.FbxModelUrl = "/Game/assets/models/SU25/SU-25.fbx";
     this.pixelsPerSecond = 25;
     this.vehicleMesh = null;
     //this.scale = 13;
@@ -9,14 +10,24 @@ class ControllableModel {
     this.vehicleColor = 0x0ffa65;
     this.rotationAngleStep = 6;
     this.group = new THREE.Object3D();
+    this.scene = null;
   }
 
-  //setGame(gameObjs){}
+  setScene(sceneObj) {
+    this.scene = sceneObj;
+  }
 
   loadFBXModel() {
-    const objs = [];
-    const loader = new THREE.FBXLoader();
+    var loader = new THREE.FBXLoader(); //new FBXLoader();
     console.log(loader);
+    console.log("here we go ...");
+    loader.load(
+      this.FbxModelUrl,
+      function(object3d) {
+        console.log("here***");
+        this.scene.add(object3d);
+      }.bind(this)
+    );
   }
 
   loadModel() {}
